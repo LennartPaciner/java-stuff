a2f5c77a137d0a5a0ac4344b208cdd05f6d14eba
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-01 21:08:27 +0100

    e2

--- Exercises/src/Exercise2.java
+++ Exercises/src/Exercise2.java
@@ -11,6 +11,15 @@ import java.util.List;
 public class Exercise2 {
 
     public List<Integer> produkt(List<Integer> liste){
+        List<Integer> array = new ArrayList<>();
+        int ergebnis = 1;
+        for(int i = 0; i < liste.size(); i++){
+            ergebnis *= liste.get(i);
+        }
+        for(int i = 0; i < liste.size(); i++){
+            array.add(ergebnis / liste.get(i));
+        }
+        System.out.print(Arrays.toString(array.toArray()).replace(", ", " ").replace("[", "").replace("]", ""));
         return liste;
     }
 
@@ -25,8 +34,10 @@ public class Exercise2 {
 
 
     public static void main(String[] args) {
+        Exercise2 exercise2 = new Exercise2();
         List<Integer> array = Arrays.asList(1, 2, 3, 4, 5);
 
+        exercise2.produkt(array);
 
     }
 }

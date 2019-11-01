9ad5821136cbce28ac844885fe5afaadc6d5e758
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-01 20:53:54 +0100

    e2

---
+++ Exercises/src/Exercise2.java
@@ -0,0 +1,32 @@
+/*
+Given an array of integers, return a new array such that each element at index i of the new array
+is the product of all the numbers in the original array except the one at i.
+*/
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise2 {
+
+    public List<Integer> produkt(List<Integer> liste){
+        return liste;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        List<Integer> array = Arrays.asList(1, 2, 3, 4, 5);
+
+
+    }
+}

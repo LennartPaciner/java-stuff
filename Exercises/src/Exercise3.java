c018cb36ba09ab794a963638c0de5265c55b0ce3
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-02 17:41:09 +0100

    e3

--- Exercises/src/Exercise3.java
+++ Exercises/src/Exercise3.java
@@ -1,2 +1,6 @@
+/*
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+Implement car and cdr.
+ */
 public class Exercise3 {
 }

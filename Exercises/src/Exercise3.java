da2c736879f2cf27ebd8b6f83a5e3696229f020f
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-03 23:20:59 +0100

    e3+e4

--- Exercises/src/Exercise3.java
+++ Exercises/src/Exercise3.java
@@ -1,6 +1,29 @@
+import javafx.util.Pair;
+
 /*
 cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 Implement car and cdr.
+generische tests
  */
 public class Exercise3 {
+
+    public <T> Pair cons(Pair<T, T> a){
+        return a;
+    }
+
+    /*public class Pair<T, U> {
+        public final T t;
+        public final U u;
+
+        public Pair(T t, U u) {
+            this.t= t;
+            this.u= u;
+        }
+    }*/
+
+
+    public static void main(String[] args) {
+
+
+    }
 }
---
+++ Exercises/src/Exercise4.java
@@ -0,0 +1,6 @@
+/*
+Eine Tree Struktur anlegen für Höhe k und den dann zeichnen lassen
+ */
+
+public class Exercise4 {
+}

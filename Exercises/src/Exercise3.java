ca0e98346bdff47d9f6c70c9dc319e7751383399
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-04 18:45:39 +0100

    e3

--- Exercises/src/Exercise3.java
+++ Exercises/src/Exercise3.java
@@ -3,7 +3,6 @@ import javafx.util.Pair;
 /*
 cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 Implement car and cdr.
-generische tests
  */
 public class Exercise3 {
 
@@ -21,6 +20,26 @@ public class Exercise3 {
         return paar;
     }
 
+    /**
+     * Bekomme erstes Element von übergebenem generischen Tupel
+     * @param a
+     * @param <T>
+     * @return erstes Element
+     */
+    public <T> T car(Pair<T, T> a){
+        return a.getKey();
+    }
+
+    /**
+     * Bekomme zweites Element von übergebenem generischen Tupel
+     * @param a
+     * @param <T>
+     * @return zweites Element
+     */
+    public <T> T cdr(Pair<T, T> a){
+        return a.getValue();
+    }
+
     /*public class Pair<T, U> {
         public final T t;
         public final U u;
@@ -34,13 +53,18 @@ public class Exercise3 {
 
     public static void main(String[] args) {
         Exercise3 exercise3 = new Exercise3();
-        
+
         //Pair(key,value). Testvariablen.
-        Pair<Integer, Integer> paar = new Pair<>(5, 6);
         String testString = "hallo";
         int testInt = 19;
         double testDouble = 5.5;
         char testChar = 'x';
+
+        //Testaufrufe
+        Pair paar = exercise3.cons(testString, testDouble);
+        Pair paar2 = exercise3.cons(testChar, testInt);
+        System.out.println(exercise3.car(paar2));
+        System.out.println(exercise3.cdr(paar2));
 
         //Print paar
         //System.out.println(exercise3.cons(testInt, testString).getKey().toString()+ " " + exercise3.cons(testInt, testString).getValue().toString());
---
+++ Exercises/src/Exercise5.java
@@ -0,0 +1,6 @@
+/*
+Einfache Datenbank erstellen und verbinden und etwas in eine Tabelle schreiben/löschen/updaten können.
+ */
+
+public class Exercise5 {
+}
